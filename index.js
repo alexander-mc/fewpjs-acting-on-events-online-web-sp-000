3e249fba601ecdf9feb9a2c8aa9071cc18d292ee
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-12-15 18:02:33 -0800

    commit tree abb67315d5000942181a395870405a15625eccb8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    let game = document.getElementById("game");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    dodger.style.left = `${left + 1}px`
+    // if (left + dodger.offsetWidth < game.offsetWidth) {
+    //   dodger.style.left = `${left + 1}px`;
+    // }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
